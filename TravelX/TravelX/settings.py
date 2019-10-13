395c56a5237ac0642720556d18534ed442e50ec8
Kishan <kishankcr7@gmail.com>
2019-10-13 19:19:21 +0530

    Inspectdb,created models (settings.py's database setting changed)

--- TravelX/TravelX/settings.py
+++ TravelX/TravelX/settings.py
@@ -77,7 +77,10 @@ WSGI_APPLICATION = 'TravelX.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': 'test.db',
+        'USER' : 'root',
+        'PASSWORD' : 'root',
+        'HOST' : 'localhost',
     }
 }
 
